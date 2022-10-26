8f04a254824ea39e7ce97746ee8e25f4c1448fa5
ding <dingZCode@163.com>
2022-10-26 17:49:34 +0800

    update account

--- scripts/account.js
+++ scripts/account.js
@@ -4,9 +4,23 @@ var ethers = require('ethers');
 
 
 const provider = ethers.getDefaultProvider('https://api.bitstack.com/v1/wNFxbiJyQsSeLrX8RRCHi7NpRxrlErZk/DjShIqLishPCTB9HiMkPHXjUM9CNM9Na/ETH/mainnet');
-console.log("block info: ",provider.getBlockNumber().then(console.log));
-console.log("wallet info:",ethers.Wallet.createRandom()._signingKey());
+
+
 //create accounts
-function batchCreateAccounts(){
-    //await signer.
+async function batchCreateAccounts(num) {
+    var accounts = '';
+    for (let index = 0; index < num; index++) {
+        const wallet = ethers.Wallet.createRandom();
+        const address = wallet.address;
+        const mnemonic = wallet._mnemonic().phrase;
+        if (index == num - 1) {
+            accounts = accounts.concat(address, '_', mnemonic);
+        } else {
+            accounts = accounts.concat(address, '_', mnemonic,',' ,'\n');
+        }
+    }
+    console.log('accounts: \n', accounts);
+
 }
+
+batchCreateAccounts(4);
