4ec6c59fc59c49be8d976b4a1f3701d00e5c6ce6
ding <dingZCode@163.com>
2022-10-27 15:43:31 +0800

    update

--- scripts/account.js
+++ scripts/account.js
@@ -3,11 +3,19 @@ var fs = require('fs');
 var ethers = require('ethers');
 var createPrivateKey = require('mnemonic-to-private-key/lib/index.js').createPrivateKey;
 
+require("dotenv").config();
+var aptos = require('aptos');
 
+const bip39 = require('bip39')
 
-
+const NODE_URL = process.env.APTOS_NODE_URL || "https://fullnode.devnet.aptoslabs.com";
+const FAUCET_URL = process.env.APTOS_FAUCET_URL || "https://faucet.devnet.aptoslabs.com";
+const aptosClient = new aptos.AptosClient(NODE_URL);
 
 const provider = ethers.getDefaultProvider('https://api.bitstack.com/v1/wNFxbiJyQsSeLrX8RRCHi7NpRxrlErZk/DjShIqLishPCTB9HiMkPHXjUM9CNM9Na/ETH/mainnet');
+
+
+
 
 
 //create accounts
@@ -28,6 +36,27 @@ async function batchCreateAccounts(num) {
 
 }
 
+async function batchCreateAccountsInAptos(num) {
+    var accounts = '';
+    for (let index = 0; index < num; index++) {
+        const mnemonic = bip39.generateMnemonic();
+        console.log("mnemonic: ", mnemonic);
+        const aptosAccount = aptos.AptosAccount.fromDerivePath("m/44'/637'/0'/0'/0'", mnemonic);
+        const address = aptosAccount.toPrivateKeyObject().address;
+        const privateKey = aptosAccount.toPrivateKeyObject().privateKeyHex;
+        console.log("address : \n", address);
+        console.log("privateKey : \n", privateKey);
+        if (index == num - 1) {
+            accounts = accounts.concat(address, '_', mnemonic, '_', privateKey);
+        } else {
+            accounts = accounts.concat(address, '_', mnemonic, '_', privateKey, ',', '\n');
+        }
+    }
+    console.log('accounts: \n', accounts);
+}
 
 
-batchCreateAccounts(5);
+
+//batchCreateAccounts(1);
+
+batchCreateAccountsInAptos(1);
